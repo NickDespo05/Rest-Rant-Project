ff72b33b1ab610630095d2f96f9fc23a65aa7ffb
Nick D'Esposito <Nicholas D'Esposito>
2022-07-08 22:20:37 -0400

    Done part 5

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,27 @@
 const router = require("express").Router();
+const places = require("../modules/places");
 
 router.get("/new", (req, res) => {
     res.render(`places/new`);
 });
 
-router.get("/:arrayIndex", (req, res) => {
-    let index = req.params.arrayIndex;
-    let places = [
-        {
-            name: "H-Thai-ML",
-            city: "Seattle",
-            state: "WA",
-            cuisines: "Thai, Pan-Asian",
-            pic: "http://placekitten.com/250/250",
-        },
-        {
-            name: "Coding Cat Cafe",
-            city: "Phoenix",
-            state: "AZ",
-            cuisines: "Coffee, Bakery",
-            pic: "http://placekitten.com/250/250",
-        },
-    ];
-
+router.get("/", (req, res) => {
     res.render(`places/index`, { places });
 });
 
 router.post("/", (req, res) => {
+    if (!req.body.pic) {
+        req.body.pic = "http://placekitten.come/400/400/";
+    }
+    if (!req.body.city) {
+        req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+        req.body.state = "MERICA BABY! (with southern accent)";
+    }
+    places.push(req.body);
     console.log(req.body);
-    res.send("POST /places");
+    res.redirect(" /places");
 });
 
 module.exports = router;
