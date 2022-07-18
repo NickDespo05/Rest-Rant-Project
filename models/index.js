efb7a9fe6075928dfdb1b36db6619e38d9a2801d
Nick D'Esposito <Nicholas D'Esposito>
2022-07-17 21:55:20 -0400

    Needed to work more on part 9

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
+require("dotenv");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+//prettier-ignore
+mongoose.connect("mongodb://localhost:27017/rest-rant", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
